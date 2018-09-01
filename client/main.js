7659a2178d899eb8f6f80fc8dcab2242cc698c9e
rldhks8745 <sdaff1004@gmail.com>
2018-09-01 17:40:23 +0000

    채팅창 UI 대폭 개선

--- client/main.js
+++ client/main.js
@@ -1,145 +1,237 @@
+const UP = 87;
+const DOWN = 83;
+const RIGHT = 68;
+const LEFT = 65;
+
+const STAND = 0;
+const WALK = 1;
+
+// 캐릭터 클래스
+function Char( name ){
+    this.name = name;
+    this.id = undefined;
+    this.x = 0;
+    this.y = 0;
+    this.prevX = 0;
+    this.prevY = 0;
+    this.act = undefined;
+    this.dir = DOWN;
+};
+
+var char;  // local 유저
+
+var users = []; // remote 유저들
+
+window.onload = function(){
+    var $loginText = $('#loginBox input[ type=text ]');
+    var $loginButton = $('#loginBox input[ type=button ]');
+
+    $loginText.keydown( function( key ){
+        if( key.keyCode == 13 ){
+            login( $loginText.val() );
+        }
+    } );
+    $loginButton.click( function(){
+        login( $loginText.val() );
+    } );
+
+    /*
+        login 후 게임, 채팅 진행
+    */
+    function login( name ){
+        if( name != "" ){
+            connectToServer( name );
+            gameInit();
+            $('#loginBox').remove();
+        }
+        else {
+            alert("이름을 입력 해주세요.");
+        }
+    }
+
+}
+
 /**************************************
           서버와 socket 연결
 **************************************/
-// dom이 다 로딩되고 난뒤 실행된다.
-$( function(){
+function connectToServer( name ){
 
-  // socket.io 서버에 접속
-  var socket = io();
+    // socket.io 서버에 접속
+    var socket = io();
 
-  // 접속 하자마자 name, userid를 서버로 전송
-  socket.emit('login', { name : makeRandomName(), userid : Math.floor( Math.random() * 1000 ) });
+    // 접속 하자마자 name, userid를 서버로 전송
+    char = new Char( name );
 
-  /******************************************
-           서버에서 전송되어온 message
-  ******************************************/
-  socket.on('login', function( data ){
-    $('#chatBox').append("<div><strong>" + data + "</strong> 이 접속했습니다.</div>");
-  });
+    socket.emit('login', char.name );
 
-  socket.on('logout', function( data ){
-    $('#chatBox').append("<div>" + data + "님이 나갔습니다.</div>");
-  });
+    /******************************************
+            서버에서 전송되어온 message
+    ******************************************/
+    socket.on('login', function( data ){
+        $('#chatBox').append("<div class='loginChat'><strong> &nbsp" + data + "</strong> 님이 접속했습니다.</div>");
+        $('#chatBox').scrollTop( $('#chatBox')[0].scrollHeight );
+    });
 
-  socket.on('chat', function( data ){
-    $('#chatBox').append("<div>" + data.from.name + "님 : " + data.msg + "</div>");
-    $('#chatBox').scrollTop( $('#chatBox')[0].scrollHeight );
-  });
-  // 서버로 메세지 전송 메소드
-  function sendMessage(){
-    var $chatText = $('#chatText input[ type=text ]');
-    socket.emit('chat', { msg : $chatText.val() });
-    $chatText.val("");
-  }
-  // text창에서 엔터 누를 시 13은 Enter
-  $('#chatText input[ type=text ]').keydown( function( key ){ if( key.keyCode == 13 ){ sendMessage(); } });
-  // 전송 버튼 클릭 시
-  $('#chatSend input[ type=button ]').click( function(){ sendMessage(); } );
+    socket.on('logout', function( data ){
+        $('#chatBox').append("<div class='logoutChat'><strong> &nbsp" + data + "</strong> 님이 나갔습니다.</div>");
+        $('#chatBox').scrollTop( $('#chatBox')[0].scrollHeight );
+    });
 
-  function makeRandomName(){
-    let name = "";
-    let possible = "abcdefghijklmnopqrstuvwxyz";
+  // 서버로 채팅 전송 메소드
+    socket.on('chat', function( data ){
+        let float;
+        if( char.name == data.name ){
+            $('#chatBox').append("<div class='chatMyself'><strong> &nbsp" + data.name + "</strong> : " + data.msg + "</div>");
+            float = 'right';
+        }
+        else{
+            $('#chatBox').append("<div class='chat'><strong> &nbsp" + data.name + "</strong> : " + data.msg + "</div>");
+            float = 'left';
+        }
+        /*
+            사용자들 대화를 block 처리 해주는
+        */
+        let height = parseFloat( window.getComputedStyle( $('#chatBox div:last').get(0) ).height ) + 18;
+        $('#chatBox').append("<div style='width:100%;height:"+height+"px;float:none;'></div>");
 
-    for( let i = 0; i < 3; i++ ){
-      name += possible.charAt( Math.floor( Math.random() * possible.length ) );
+        $('#chatBox').scrollTop( $('#chatBox')[0].scrollHeight );
+    });
+    function sendMessage(){
+        var $chatText = $('#chatText input[ type=text ]');
+        socket.emit('chat', { msg : $chatText.val() });
+        $chatText.val("");
     }
+    // text창에서 엔터 누를 시 13은 Enter
+    $('#chatText input[ type=text ]').keydown( function( key ){ if( key.keyCode == 13 ){ sendMessage(); } });
+    // 전송 버튼 클릭 시
+    $('#chatSend input[ type=button ]').click( function(){ sendMessage(); } );
 
-    return name;
-  }
+    socket.on('update', function( data ){ // 현재 자신과 같은 맵에 있는 유저들 data
+        users = data;
+    });
 
-});
+    // 자신 정보 서버로 update 루프
+    var updateId = setInterval( function(){
+        socket.emit('update', char );
+    }, 16);
+}
 
 /**************************************
-              게임 canvas
+                게임 canvas
 ***************************************/
-$( function(){
+function gameInit(){
+    var canvas = document.getElementById("myCanvas"); // 캔버스 문서객체
+    var ctx = canvas.getContext("2d"); // 2d 캔버스 얻어오기
 
-      const UP = 87;
-      const DOWN = 83;
-      const RIGHT = 68;
-      const LEFT = 65;
+    var upPressed = false;
+    var downPressed = false;
+    var rightPressed = false;
+    var leftPressed = false;
 
-      var canvas = document.getElementById("myCanvas"); // 캔버스 문서객체
-      var ctx = canvas.getContext("2d"); // 2d 캔버스 얻어오기
+    var charImg = new Image();
+    charImg.src = "/images/char.png";
+    charImg.onload = function(){ char.act = STAND };
 
-      var upPressed = false;
-      var downPressed = false;
-      var rightPressed = false;
-      var leftPressed = false;
-
-      var charX = 25;
-      var charY = 25;
-
-      // 키 눌렀을때
-      function keyDownHandler(e)
-      {
+    // 키 눌렀을때
+    function keyDownHandler(e)
+    {
         if( document.activeElement.tagName != "INPUT" ){ // INPUT 태그가 포커스 되있으면 캐릭터가 움직이지 않게
-            if( e.keyCode == UP )
+            if( e.keyCode == UP ){
                 upPressed = true;
-            if( e.keyCode == DOWN )
+                char.dir = UP;
+                char.act = WALK;
+            }
+            if( e.keyCode == DOWN ){
                 downPressed = true;
-            if( e.keyCode == RIGHT )
+                char.dir = DOWN;
+                char.act = WALK;
+            }
+            if( e.keyCode == RIGHT ){
                 rightPressed = true;
-            if( e.keyCode == LEFT )
+                char.dir = RIGHT;
+                char.act = WALK;
+            }
+            if( e.keyCode == LEFT ){
                 leftPressed = true;
+                char.dir = LEFT;
+                char.act = WALK;
+            }
         }
-      }
-      // 키 누르고 땟을 때
-      function keyUpHandler(e)
-      {
-        if( document.activeElement.tagName != "INPUT" ){
-          if( e.keyCode == UP )
+  }
+  // 키 누르고 땟을 때
+  function keyUpHandler(e)
+  {
+      if( document.activeElement.tagName != "INPUT" ){
+          if( e.keyCode == UP ){
               upPressed = false;
-          if( e.keyCode == DOWN )
+          }
+          if( e.keyCode == DOWN ){
               downPressed = false;
-          if( e.keyCode == RIGHT )
+          }
+          if( e.keyCode == RIGHT ){
               rightPressed = false;
-          if( e.keyCode == LEFT )
+          }
+          if( e.keyCode == LEFT ){
               leftPressed = false;
+          }
+
+          if( upPressed == false && downPressed == false && rightPressed == false && leftPressed == false)
+            char.act = STAND;
+        }
+        else {
+            upPressed = false;
+            downPressed = false;
+            rightPressed = false;
+            leftPressed = false;
+        }
+  }
+  function mouseMoveHandler(e)
+  {
+
+  }
+
+  /*****************************
+          draw 메소드
+  *****************************/
+  function draw()
+  {
+      // 화면 클리어
+      ctx.clearRect( 0,0, canvas.width, canvas.height );
+
+      // Draw
+      ctx.beginPath();
+
+      if( char.act != undefined ) // ctx.drawImage(image,sourcex,sourcey,sourcewidth,sourceheight,drawx,drawy,drawwidth,drawheight);
+        ctx.drawImage( charImg, 0,0,64,128, char.x,char.y,64,128 );
+
+      for( let i=0; i<users.length; i++ )
+      {
+        if( users[i].name != char.name )
+        {
+          ctx.drawImage( charImg, 0,0,64,128, users[i].x,users[i].y,64,128 );
         }
       }
-      function mouseMoveHandler(e)
-      {
 
-      }
+      ctx.closePath();
 
-      /*****************************
-              draw 메소드
-      *****************************/
-      function draw()
-      {
-          // 화면 클리어
-          ctx.clearRect( 0,0, canvas.width, canvas.height );
+      // key값 검사
+      if( upPressed )
+          char.y -= 5;
+      if( downPressed )
+          char.y += 5;
+      if( rightPressed )
+          char.x += 5;
+      if( leftPressed )
+          char.x -= 5;
 
-          // Draw
-          ctx.beginPath();
-          ctx.fillStyle = "red";
-          ctx.arc( charX, charY, 5, 0, Math.PI*2 );
-          ctx.fill();
-          ctx.closePath();
+      requestAnimationFrame(draw); // draw 쟤요청
+  }
 
-          // key값 검사
-          if( upPressed )
-              charY -= 5;
-          if( downPressed )
-              charY += 5;
-          if( rightPressed )
-              charX += 5;
-          if( leftPressed )
-              charX -= 5;
+  // draw 메소드 한번 동작 시키기
+  draw();
 
-
-          requestAnimationFrame(draw); // draw 쟤요청
-
-
-      }
-
-      // draw 메소드 한번 동작 시키기
-      draw();
-
-      // 핸들러 등록
-      document.addEventListener("keydown", keyDownHandler, false);
-      document.addEventListener("keyup", keyUpHandler, false);
-      document.addEventListener("mousemove", mouseMoveHandler, false);
-
-});
+  // 핸들러 등록
+  document.addEventListener("keydown", keyDownHandler, false);
+  document.addEventListener("keyup", keyUpHandler, false);
+  document.addEventListener("mousemove", mouseMoveHandler, false);
+}
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+
+// 캐릭터 클래스
+function Char( name ){
+    this.name = name;
+    this.x = 0;
+    this.y = 0;
+    this.prevX = 0;
+    this.prevY = 0;
+    this.act = undefined;
+    this.dir = undefined;
+};
+
 var express = require('express') // 웹 서버 플랫폼
 , http = require('http') // http 모듈
 , fs = require('fs') // 파일 로드
@@ -6,18 +18,22 @@ var express = require('express') // 웹 서버 플랫폼
 , server = http.createServer( app ).listen( 80, function(){ console.log( "Server Running" ) } );
 
 // http 서버를 socket.io 서버로 업그레이드
-var io = require('socket.io')(server);
+var io = require('socket.io')(server, { pingTimeout:1000, pingInterval:5000, upgradeTimeout:2000 });
+//, redisAdapter = require('socket.io-redis');
+//io.adapter( redisAdapter({ host:'localhost', port : 6379}));
 
 // /client로 url 요청 시 '__dirname(현재위치)/client' 에서 찾는다는것임
 app.use( '/client', express.static(__dirname + '/client') );
+app.use( '/images', express.static(__dirname + '/images') );
+app.use( '/js', express.static(__dirname + '/js') );
 
 var CLIENT_DIR = "./client";
 
 // index.html 전송
 app.get( "/", function( request, response ){
-	fs.readFile( CLIENT_DIR + "/index.html", function( err, data ){
+    fs.readFile( CLIENT_DIR + "/index.html", function( err, data ){
 		if( err ){
-			response.writeHead( 404 );
+            response.writeHead( 404 );
 			response.end( JSON.stringify( err ) );
 			return;
 		}
@@ -27,45 +43,72 @@ app.get( "/", function( request, response ){
 	} );
 } );
 
+var clients = new Map();
+
 // 클라이언트가 socket.io 서버에 접속 했을 때 connection 이벤트 발생
 // connection event handler function 인자로 socket가 전달됨
 // socket는 개별 클라이언트와 상호작용 객체.
 // io는 전체 클라이언트와으 상호작용 객체이다.
 io.on('connection', function( socket ){
-	let clientName;
 	// 클라이언트 접속 메세지 날리면
 	socket.on('login', function( data ){
-		console.log( "Client logged-in : " + data.name + ", userid : " + data.userid );
-		clientName = data.name;
 
-		//socket에 클라이언트 정보 저장
-		socket.name = data.name;
-		socket.userid = data.userid;
+    socket.name = data;
+    socket.join('mainroom'); // 기본적을 manroom 에 참가
+    clients.set( socket.id, new Char( data ) ); // 소켓id로 key값
 
 		// 접속된 모든 클라이언트에게 메세지 전송
-		io.emit('login', data.name);
+		io.emit('login', data);
 	});
 
 	socket.on('chat', function( data ){
 		console.log("Message from %s : %s", socket.name, data.msg );
 
 		var msg = {
-			from: {
-				name : socket.name,
-				userid : socket.userid
-			},
+			name : socket.name,
 			msg : data.msg
 		};
 
 		io.emit('chat', msg);
 	});
 
-	socket.on('forceDisconnect', function(){
-		socket.disconnect();
+	socket.on('update', function( data ){
+
+    if( clients.has( socket.id ) ){
+        var char = new Char( data.name );
+		char.prevX = clients.get( socket.id ).x;
+		char.prevY = clients.get( socket.id ).y;
+		char.x = data.x;
+		char.y = data.y;
+		char.act = data.act;
+		char.dir = data.dir;
+
+        clients.set( socket.id, char );
+    }
+
 	});
 
-	socket.on('disconnect', function(){
-		socket.broadcast.emit('logout', socket.name );
-		console.log("user disconnected : " + socket.name );
+	socket.on('forceDisconnect', function(){
+		socket.disconnect();
+		console.log(" forceDisconnect")
 	});
+    socket.on('disconnect', function(){
+        if( clients.has( socket.id ) ){
+            socket.broadcast.emit('logout', clients.get( socket.id ).name ); // 자신을 제외하고 다른 clients에게 logout message 보내기
+            console.log("user disconnected : " + clients.get( socket.id ).name ); // log
+
+            clients.delete( socket.id ); // 서버 메모리에서 관리하는 clients 들에서 삭제
+        }
+    });
 });
+
+
+
+var intervalId = setInterval( function(){
+    var data = [];
+    for( var value of clients.values() ){
+        data.push( value ); // Map 자료구조로 안보내져서 Array로 재 생성후 보내기
+    }
+
+	io.volatile.emit('update', data );
+}, 8);
